06f36f017b00893b73d119602f0d9899a48c66a6
Simon Gubler <s_gubler@bluemail.ch>
2015-05-16 08:27:10 +0200

    added unhandled exception handler

--- SqlServerValidationToolkit.Configurator/App.xaml.cs
+++ SqlServerValidationToolkit.Configurator/App.xaml.cs
@@ -13,5 +13,12 @@ namespace SqlServerValidationToolkit.Configurator
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var baseException = e.Exception.GetBaseException();
+            string message = string.Format("{0}" + Environment.NewLine + "at {1}", baseException.Message, baseException.StackTrace);
+            MessageBox.Show(message, "Unhandled Exception occured");
+            e.Handled = true;
+        }
     }
 }
