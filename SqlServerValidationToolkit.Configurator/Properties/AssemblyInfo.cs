cd04cc59f5347b588a400bd3d2c7849003bf6121
Simon Gubler <s_gubler@bluemail.ch>
2015-07-23 21:35:25 +0200

    Configurator loggs to logfile

--- SqlServerValidationToolkit.Configurator/Properties/AssemblyInfo.cs
+++ SqlServerValidationToolkit.Configurator/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using log4net.Config;
+using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -15,6 +16,7 @@ using System.Windows;
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: XmlConfigurator]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
