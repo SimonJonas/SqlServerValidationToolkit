a310bfa9fc4e3857b33370e112058ff76a7fad63
Simon Gubler <s_gubler@bluemail.ch>
2015-08-02 11:49:30 +0200

    A message is shown if the sdf-file is created by another user

--- SqlServerValidationToolkit.Configurator/App.xaml.cs
+++ SqlServerValidationToolkit.Configurator/App.xaml.cs
@@ -3,6 +3,7 @@ using SqlServerValidationToolkit.Configurator.Properties;
 using SqlServerValidationToolkit.Model.Context;
 using SqlServerValidationToolkit.Model.DatabaseInitialization;
 using SqlServerValidationToolkit.Model.Entities;
+using SqlServerValidationToolkit.Model.Validation;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -27,24 +28,40 @@ namespace SqlServerValidationToolkit.Configurator
                 if (!ctx.Databases.Any())
                 {
                     MessageBox.Show("Welcome to the Validation Toolkit. Please select the database to validate", "Welcome", MessageBoxButton.OK);
+
+                    SetDbConnectionString();
                     
-                    ConnectionStringUpdater.UpdateDbConnectionString(
-                    () =>
+                }
+                else
+                {
+                    var decryptedConnectionString = Validator.GetDecryptedConnectionString();
+                    if (decryptedConnectionString == null || decryptedConnectionString == string.Empty)
                     {
-                    },
-                    (ex) =>
-                    {
-                        _log.Error("Exception occurred while updating the connection string", ex);
-                        MessageBox.Show(string.Format("An error occurred: '{0}'" + Environment.NewLine + "The application will shut down. Please start it again and select another database.", ex.Message), "Validation toolkit", MessageBoxButton.OK, MessageBoxImage.Error);
-                        App.Current.Shutdown();
+
+                        _log.Error("Error occurred while decrypting the connection string, connection string will be reset by the current user");
+                        MessageBox.Show("Welcome to the Validation Toolkit. The current local database file was created by another user. Please select the connection string again for the validated database.", "Welcome", MessageBoxButton.OK);
+                        SetDbConnectionString();
                     }
-                    );
-                    
                 }
             }
 
             base.OnStartup(e);
 
+        }
+
+        private void SetDbConnectionString()
+        {
+            ConnectionStringUpdater.UpdateDbConnectionString(
+            () =>
+            {
+            },
+            (ex) =>
+            {
+                _log.Error("Exception occurred while updating the connection string", ex);
+                MessageBox.Show(string.Format("An error occurred: '{0}'" + Environment.NewLine + "The application will shut down. Please start it again and select another database.", ex.Message), "Validation toolkit", MessageBoxButton.OK, MessageBoxImage.Error);
+                App.Current.Shutdown();
+            }
+            );
         }
 
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
